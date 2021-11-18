b28266461fd0399a7cb736ff0edc259ece1a4d04
Bernardo Meurer <bernardo@meurer.org>
2021-11-17 18:49:08 -0800

    refactor: use structopt for cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,39 @@
-use std::process;
-
-use anyhow::{Context, Result};
-use clap::{App, Arg, ArgMatches, SubCommand};
+use anyhow::{bail, Context, Result};
 use mdbook::preprocess::Preprocessor;
+use structopt::StructOpt;
 
 mod preprocessor;
 mod svgbob;
 
-const VERSION: &str = env!("CARGO_PKG_VERSION");
-const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
-const NAME: &str = env!("CARGO_PKG_NAME");
-const DESCRIPTION: &str = concat!(
-    env!("CARGO_PKG_DESCRIPTION"),
-    "\n",
-    env!("CARGO_PKG_HOMEPAGE")
-);
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Option<SubCommand>,
+}
 
-pub fn init_cli() -> Result<ArgMatches<'static>> {
-    env_logger::try_init().with_context(|| "failed to initialize logger")?;
-
-    Ok(App::new(NAME)
-        .version(VERSION)
-        .author(AUTHORS)
-        .about(DESCRIPTION)
-        .subcommand(
-            SubCommand::with_name("supports")
-                .arg(Arg::with_name("renderer").required(true))
-                .about("Check whether a renderer is supported by this preprocessor"),
-        )
-        .get_matches())
+#[derive(Debug, StructOpt)]
+enum SubCommand {
+    #[structopt(help = "Check whether a renderer is supported by this preprocessor")]
+    Supports { renderer: String },
 }
 
 fn main() -> Result<()> {
-    let opts = init_cli().with_context(|| "failed to initialize cli")?;
+    env_logger::try_init().with_context(|| "failed to initialize logger")?;
+
     let bob = preprocessor::Bob::new();
 
-    // handle supports or processsing:
-    if let Some(sub_args) = opts.subcommand_matches("supports") {
-        let renderer = sub_args.value_of("renderer").expect("Required argument");
-        let supported = bob.supports_renderer(renderer);
-
-        // Signal whether the renderer is supported by exiting with 1 or 0.
-        if supported {
-            process::exit(0);
-        } else {
-            process::exit(1);
+    match Opt::from_args().cmd {
+        // checks whether or not a given renderer is supported
+        Some(SubCommand::Supports { renderer }) => {
+            if bob.supports_renderer(&renderer) {
+                Ok(())
+            } else {
+                bail!("svgbob does not support this renderer")
+            }
         }
-    } else if let Err(e) = bob.handle_preprocessing() {
-        log::error!("{}", e);
-        process::exit(1);
+        // preprocesses the input
+        None => bob
+            .handle_preprocessing()
+            .with_context(|| "failed to preprocess svgbob drawing"),
     }
-    Ok(())
 }
